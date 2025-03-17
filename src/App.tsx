d5e5d5315409f95c4428b7fabd88ca40c29f72d7
홍재훈 <ehswkfn123@naver.com>
2025-03-17 19:52:51 +0900

    ui : selectDifference 컴포넌트 기본 디자인

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Main from "./components/Main";
+import SelectDifference from "./components/SelectDifference";
 import { useAppDispatch } from "./hooks/useAppDispatch";
 import { useAppSelector } from "./hooks/useAppSelector";
 import AppWrapper from "./layout/AppWrapper";
 import { selectMode, setMode } from "./slice/modeSlice";
+import { AnimatePresence } from "framer-motion";
 
 const App = () => {
   const currentMode = useAppSelector(selectMode);
@@ -13,14 +15,17 @@ const App = () => {
     dispatch(setMode("main"));
   };
 
-  const gameButtonHandler = () => {
-    dispatch(setMode("game"));
-  };
-
   return (
     <AppWrapper>
-      {currentMode === "main" && <Main />}
-      {currentMode === "game" && <div>게임화면입니다</div>}
+      <AnimatePresence mode='wait'>
+        {currentMode === "main" && <Main />}
+      </AnimatePresence>
+      <AnimatePresence mode='wait'>
+        {currentMode === "selectDifference" && <SelectDifference />}
+      </AnimatePresence>
+      <AnimatePresence>
+        {currentMode === "game" && <div>게임화면입니다</div>}
+      </AnimatePresence>
       {currentMode === "docs" && <div>도감화면입니다</div>}
     </AppWrapper>
   );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,12 @@
 import SelectButton from "../ui/SelectButton";
 import { useAppDispatch } from "../hooks/useAppDispatch";
 import { setMode } from "../slice/modeSlice";
+
 const Main = () => {
   const dispatch = useAppDispatch();
 
-  const gameButtonHandler = () => {
-    dispatch(setMode("game"));
+  const selectDifferenceButtonHandler = () => {
+    dispatch(setMode("selectDifference"));
   };
 
   const docsButtonHandler = () => {
@@ -14,8 +15,15 @@ const Main = () => {
   return (
     <div className='absolute bottom-[120px] left-1/2 -translate-x-1/2'>
       <div className='flex flex-col gap-5'>
-        <SelectButton onMode={gameButtonHandler}>게임시작</SelectButton>
-        <SelectButton onMode={docsButtonHandler}>도감보기</SelectButton>
+        <SelectButton
+          onMode={selectDifferenceButtonHandler}
+          buttonColor='#181818'
+        >
+          게임시작
+        </SelectButton>
+        <SelectButton onMode={docsButtonHandler} buttonColor='#181818'>
+          도감보기
+        </SelectButton>
       </div>
     </div>
   );
