d10b7e097f3ba365b565b5d45ac935ffed23a3f3
홍재훈 <ehswkfn123@naver.com>
2025-03-19 04:26:47 +0900

    refactor : 타이머 부모 컴포넌트로 위치

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,19 +1,42 @@
-import SelectDifference from "./SelectDifference";
 import { useAppSelector } from "../hooks/useAppSelector";
-import { selectMode } from "../slice/modeSlice";
 import { shuffledPokemons } from "../slice/pokemonSlice";
+import { useState, useEffect } from "react";
+import { useAppDispatch } from "../hooks/useAppDispatch";
+import { setAllFliedTrue } from "../slice/pokemonSlice";
 
-import { useState } from "react";
 import GameCard from "./GameCard";
 const GameBoard = () => {
-  const currentMode = useAppSelector(selectMode);
   const shuffleCards = useAppSelector(shuffledPokemons);
-  console.log(shuffleCards[0]);
+  const [isClickEnabled, setIsClickEnabled] = useState(false); // 클릭 활성화 상태
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const flipTimer = setTimeout(() => {
+      dispatch(setAllFliedTrue()); // 3초 후 카드가 setFlipped뒤집힘
+    }, 3000);
+
+    const clickTimer = setTimeout(() => {
+      setIsClickEnabled(true); // 4초 후 클릭 가능
+    }, 4000);
+
+    return () => {
+      clearTimeout(flipTimer);
+      clearTimeout(clickTimer); // 타이머 정리
+    };
+  }, [dispatch]);
+
   return (
     <div>
       <div className='grid grid-cols-5'>
         {shuffleCards.map((card) => {
-          return <GameCard {...card} key={card.uniqueId} />;
+          return (
+            <GameCard
+              {...card}
+              key={card.uniqueId}
+              isClickEnabled={isClickEnabled}
+            />
+          );
         })}
       </div>
     </div>
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,44 +1,20 @@
 import { motion } from "framer-motion";
-import { useState, useEffect } from "react";
 import { useAppDispatch } from "../hooks/useAppDispatch";
-import { setAllFliedTrue, setFlipCard } from "../slice/pokemonSlice";
+import { setFlipCard } from "../slice/pokemonSlice";
 
 const GameCard: React.FC<{
   isFlied: boolean;
   pokemonName: string;
   number: number;
   uniqueId: string;
-}> = ({ isFlied, pokemonName, number, uniqueId }) => {
-  const [isClickEnabled, setIsClickEnabled] = useState(false); // 클릭 활성화 상태
-  const [selectCards, setSelectCards] = useState<
-    { pokemonName: string; uniqueId: string }[]
-  >([]);
-
+  isClickEnabled: boolean;
+}> = ({ isFlied, pokemonName, number, uniqueId, isClickEnabled }) => {
   const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    const flipTimer = setTimeout(() => {
-      dispatch(setAllFliedTrue()); // 3초 후 카드가 setFlipped뒤집힘
-    }, 3000);
-
-    const clickTimer = setTimeout(() => {
-      setIsClickEnabled(true); // 4초 후 클릭 가능
-    }, 4000);
-
-    return () => {
-      clearTimeout(flipTimer);
-      clearTimeout(clickTimer); // 타이머 정리
-    };
-  }, [dispatch]);
-
   const handleCardClick = () => {
-    console.log(isFlied);
-    // Only allow adding the card to the selectCards if clicking is enabled
     if (isClickEnabled) {
       dispatch(setFlipCard(uniqueId));
     }
   };
-  console.log(selectCards);
   return (
     <>
       <motion.div
