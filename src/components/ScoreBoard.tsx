141ecd4e0dced1fe22d1b0434b97c88ee0f68dbf
홍재훈 <ehswkfn123@naver.com>
2025-03-22 09:59:11 +0900

    ui : 스코어 보드 디자인

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -4,9 +4,18 @@ import { currentScore } from "../slice/scoreSlice";
 const ScoreBoard = () => {
   const score = useAppSelector(currentScore);
   return (
-    <>
-      <h2>Score : {score}</h2>
-    </>
+    <div className='absolute md:top-16 right-1/2 translate-x-1/2 top-[200px]'>
+      <div className='relative md:w-[180px] md:h-[100px] w-[120px] h-[70px] bg-white border-12 border-black rounded-md shadow-md flex flex-col items-center'>
+        <div className='absolute -top-3 bg-white px-3 py-1 text-black text-sm font-bold'>
+          SCORE
+        </div>
+        <div className='w-full h-full flex items-center justify-center border-4 border-black bg-white'>
+          <span className='text-black md:text-[48px] text-3xl font-bold '>
+            {score}
+          </span>
+        </div>
+      </div>
+    </div>
   );
 };
 
