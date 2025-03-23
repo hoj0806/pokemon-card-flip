8e62448e073e7b3ed70d14ddee0f4561188d8d55
홍재훈 <ehswkfn123@naver.com>
2025-03-24 01:58:51 +0900

    ui : 세부 디자인 수정

--- src/components/PokedexDetail.tsx
+++ src/components/PokedexDetail.tsx
@@ -80,19 +80,43 @@ const PokemonDetail: React.FC<{
       {/* Wrapper with blur effect */}
       <div
         ref={cardRef}
-        className='w-[400px] h-[600px] bg-white font-bold p-4 text-right text-[#181a1a] rounded-lg shadow-sm relative transition-all duration-300 ease-out'
-        style={{ transform }}
+        className='w-[400px] h-[600px] font-bold p-4 text-right text-[#181a1a] rounded-lg shadow-lg relative transition-all duration-300 ease-out'
+        style={{
+          transform,
+          background: `linear-gradient(
+      145deg,
+#FF3B30 0%,     /* 밝은 빨간색 (rgba(255, 59, 48, 1)) */
+  #F7352B 10%,    /* 보간된 색상 */
+  #EF2F26 20%,    /* 보간된 색상 */
+  #E63228 25%,    /* 중간 빨간색 (rgba(230, 50, 40, 1)) */
+  #DE2C22 30%,    /* 보간된 색상 */
+  #D6281E 40%,    /* 보간된 색상 */
+  #C8281E 50%,    /* 어두운 빨간색 (rgba(200, 40, 30, 1)) */
+  #C0241A 60%,    /* 보간된 색상 */
+  #B82016 70%,    /* 보간된 색상 */
+  #B41E14 75%,    /* 더 어두운 빨간색 (rgba(180, 30, 20, 1)) */
+  #AC1A10 80%,    /* 보간된 색상 */
+  #A4160C 90%,    /* 보간된 색상 */
+  #96140A 100%    /* 가장 어두운 빨간색 (rgba(150, 
+    )`,
+          boxShadow: transform
+            ? "0 20px 30px rgba(0, 0, 0, 0.3), inset 0 0 10px rgba(255, 255, 255, 0.5)" // hover 시 그림자 강조
+            : "0 10px 20px rgba(0, 0, 0, 0.2), inset 0 0 5px rgba(255, 255, 255, 0.3)", // 기본 그림자
+        }}
         onClick={(e) => e.stopPropagation()} // 카드 클릭 시 이벤트 전파 방지
       >
         {/* Glow effect */}
         <div
           className='absolute inset-0 w-full h-full pointer-events-none rounded-lg'
           style={{
-            backgroundImage: `radial-gradient(
-              circle at ${glowPosition},
-              rgba(255, 255, 255, 0.33),
-              rgba(0, 0, 0, 0.06)
-            )`,
+            backgroundImage:
+              glowPosition !== "50% 50%" // glowPosition이 초기값이 아닐 때만 glow 효과 적용
+                ? `radial-gradient(
+          circle at ${glowPosition},
+          rgba(255, 255, 255, 0.33),
+          rgba(0, 0, 0, 0.06)
+        )`
+                : "none", // 초기값일 때는 glow 효과 없음
           }}
         />
 
@@ -106,12 +130,7 @@ const PokemonDetail: React.FC<{
         </div>
 
         {/* Pokemon Name with Glow Effect */}
-        <div
-          className='absolute bottom-4 right-4 text-xl font-bold'
-          style={{
-            textShadow: `0 0 10px rgba(255, 255, 255, 0.8), 0 0 20px rgba(255, 255, 255, 0.6)`,
-          }}
-        >
+        <div className='absolute bottom-4 right-4 text-xl font-bold'>
           {selectPokemonData.pokemonName}
         </div>
       </div>
